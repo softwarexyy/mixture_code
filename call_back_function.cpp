21bfc3a62c47ec51b61c184be553064c094bdb37
Yancy Xerxes <704597765@qq.com>
2016-09-19 16:43:25 +0800

    Create call_back_function.cpp

---
+++ call_back_function.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+typedef void (*Callbackfunc1)(int* a, int* b);      //定义函数指针
+typedef bool (*Callbackfunc2)(int a, int b);
+
+void mySwap(int *a, int *b);
+bool myCompare(int a, int b);
+
+//Callbackfunc1 swapxyy = &mySwap;
+//Callbackfunc2 comparexyy = &myCompare;
+void printSmall(int a, int b, Callbackfunc2 callbacktest);  //利用函数指针
+
+int main()
+{
+    int a,b;
+    cin >> a >> b;
+    printSmall(a, b, &myCompare);             //回调函数
+}
+
+void printSmall(int a, int b, Callbackfunc2 callbacktest){
+    if(callbacktest(a, b) == true)
+        cout << a;
+    else
+        cout << b;
+}
+
+
+void mySwap(int *a, int *b){
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+    cout << "mySwap" << endl;
+}
+
+bool myCompare(int a, int b){
+    if (a<b)
+        return true;
+    else
+        return false;
+}
